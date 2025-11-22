eb7cc5eb3afcb5b8904557bf7e58690bd2498c3a
grister <kravchenko.cotleto@gmail.com>
2025-11-22 14:51:54 +0200

    [FEAT] (cats.models) create Cat model

--- cats/models.py
+++ cats/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Cat(models.Model):
+    name = models.CharField(max_length=128)
+    years_of_experience = models.PositiveIntegerField()
+    breed = models.CharField(max_length=128)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+
+    active_mission = models.ForeignKey(
+        'missions.Mission',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='assigned_cat'
+    )
+
+    def __str__(self):
+        return self.name
